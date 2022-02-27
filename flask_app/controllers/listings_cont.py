25335ff7332662ab5574a0fdb6c3457a5b74d48d
Jesse Duvall <jesserduvall@outlook.com>
2022-02-26 20:09:53 -0800

    commit users and listings

---
+++ flask_app/controllers/listings_cont.py
@@ -0,0 +1,84 @@
+from flask_app import app
+from flask import render_template, redirect, request, session
+from flask_app.models.lisiting_mod import Listings
+from flask_app.models.user_mod import Users
+
+# Add template for "new listing"
+@app.route('/new_listing/<int:userID>')
+def create_listing(userID):
+    if 'user_id' not in session:
+        return redirect('/')
+    userID = session['user_id']
+    return render_template('new_listing.html', current_user = userID)
+
+@app.route('/process_listing/<int:userID>', methods=['POST'])
+def process_listing(userID):
+    userID = session['userID']
+    if not Listings.validate_listing(request.form):
+        # Add template for "new listing"
+        return redirect(f'/new_listing/{userID}') 
+    data = {
+        'title': request.form['name'],
+        'description': request.form['description'],
+        'listPrice': request.form['listPrice'],
+        'user_id': session['user_id']
+    }
+    Listings.save_listing(data)
+    # Add template for "profile" page
+    return redirect(f'/profile/{userID}')
+
+@app.route('/listings/<int:listingID>')
+def all_listings(listingID):
+    if 'user_id' in session:
+        user_data = {
+            'id': session['userID']
+        }
+        list_data = {
+            'listingID': listingID
+        }
+        one_user = Users.get_profile(user_data)
+        all_listings = Listings.get_listings(list_data)
+        return render_template('listings.html', current_user = one_user, listings = all_listings)
+    else:
+        list_data = {
+            'listingID': listingID
+        }
+        all_listings = Listings.get_listings(list_data)
+        return render_template('listings.html', current_user = False, session = 0, listings = all_listings)
+
+@app.route('/edit_listing/<int:userID>/<listingID>')
+def edit_listing(userID, listingID):
+    userID = session['userID']
+    data = {
+        'id': listingID
+    }
+    one_listing = Listings.get_listings(data)
+    return render_template('edit_listing.html', current_user = userID, listing = one_listing)
+
+@app.route('/update_listing/<int:userID>/<listingID>', methods=['POST'])
+def update_listing(userID, productID):
+    userID = session['userID']
+    if 'userID' not in session:
+        return redirect('/')
+    if not Listings.validate_edit(request.form):
+        return redirect(f'/edit_listing/{userID}/{productID}')
+    data = {
+        'id': productID,
+        'title': request.form['title'],
+        'description': request.form['description'],
+        'listPrice': request.form['listPrice'],
+        'user_id': session['user_id']
+    }
+    Listings.update_listing(data)
+    return redirect(f'/profile/{userID}')
+
+@app.route('/delete_listing/<int:userID>/<listingID>', methods=['POST'])
+def delete_listing(userID, listingID):
+    userID = session['userID']
+    if 'user_id' not in session:
+        return redirect('/')
+    data = {
+        'id': listingID
+    }
+    Listings.delete_listing(data)
+    return redirect(f'/profile/{userID}')
---
+++ flask_app/models/lisiting_mod.py
@@ -0,0 +1,104 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+from flask_app.models import user_mod
+
+class Listings:
+    db_name = "Home_Listing_Project_ERD"
+    def __init__(self, data):
+        self.id = data['id']
+        self.title = data['title']
+        self.description = data['description']
+        self.listPrice = data['listPrice']
+        self.imgURL = data['imgURL']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.user = []
+
+# Confirm fields are filled out
+    @staticmethod
+    def validate_listing(listing):
+        is_valid = True
+        if len(listing['title']) < 1:
+            flash("Please provide a title for this listing.")
+            is_valid = False
+        if len(listing['description']) < 20:
+            flash("PLease provide a description longer than 20 characters.")
+            is_valid = False
+        if len(listing['listPrice']) < 0.01:
+            flash("Please provide a price for this listing.")
+            is_valid = False
+        return is_valid
+
+# Confirm edit fields are still filled out
+    @staticmethod
+    def validate_edit(listing):
+        is_valid = True
+        if len(listing['title']) < 1:
+            flash("Please provide a title for this listing.")
+            is_valid = False
+        if len(listing['description']) < 20:
+            flash("PLease provide a description longer than 20 characters.")
+            is_valid = False
+        if len(listing['listPrice']) < 0.01:
+            flash("Please provide a price for this listing.")
+            is_valid = False
+        return is_valid
+
+# Save new home listing
+    @classmethod
+    def save_listing(cls, data):
+        query = "INSERT INTO listings (title, description, listPrice, created_at, updated_at, user_id) VALUES (%(title)s, %(description)s, %(listPrice)s, NOW(), NOW(), %(user_id)s);"
+        results = connectToMySQL(cls.db_name).query_db(query, data)
+        return results
+
+# View all home listings associated with user_id
+    @classmethod
+    def user_listings(cls):
+        query = "SELECT * FROM users LEFT JOIN listings ON user_id = users.id;"
+        results = connectToMySQL(cls.db_name).query_db(query)
+        all_listings = []
+        for db_row in results:
+            listing_data = {
+                'id': db_row['listings.id'],
+                'title': db_row['title'],
+                'description': db_row['description'],
+                'listPrice': db_row['listPrice'],
+                'created_at': db_row['created_at'],
+                'updated_at': db_row['updated_at']
+            }
+            one_listing = cls(listing_data)
+            user_data = {
+                'id': db_row['id'],
+                'first_name': db_row['first_name'],
+                'last_name': db_row['last_name'],
+                'email': db_row['email'],
+                'password': db_row['password'],
+                'created_at': db_row['created_at'],
+                'updated_at': db_row['updated_at']
+            }
+            one_user = user_mod.Users(user_data)
+            one_listing.user = one_user
+            all_listings.append(one_listing)
+        return all_listings
+
+# Get all home listings, for public listing page
+    @classmethod
+    def get_listings(cls, data):
+        query = "SELECT * FROM listings WHERE id = %(id)s;"
+        results = connectToMySQL(cls.db_name).query_db(query, data)
+        if not results:
+            return False
+        return cls(results[0])
+
+# Edit home listing
+    @classmethod
+    def update_listing(cls, data):
+        query = "UPDATE listings SET name=%(name)s, prod_desc=%(description)s, price=%(listPrice)s, updated_at=NOW() WHERE id = %(id)s"
+        results = connectToMySQL(cls.db_name).query_db(query, data)
+        return results
+
+# Delete home listing
+    @classmethod
+    def delete_listing(cls, data):
+        query = "DELETE FROM listings WHERE id = %(id)s;"
+        return connectToMySQL(cls.db_name).query_db(query, data)
